db6eb8b9106d346fc5da9795ed6eb940f59db794
lauratamath <laurylevi@hotmail.com>
2020-01-19 10:57:28 -0600

    ya estan los metodos establecidos, pero no terminados

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@
 
 public class Main{
 	public static void main (String args []){
-		
+		 Scanner sc = new Scanner(System.in);
+		 RadioLo radio = new RadioLo();
+		 
 	}
 }
--- RadioLo.java
+++ RadioLo.java
@@ -8,9 +8,42 @@ public class RadioLo{
 	private String[] estacion; //Indica las estacione existentes
 	private String[] favFM; //guarda las estaciones favoritas de Fm
 	private String[] favAm; //guarda las estaciones favoritas de Am
-	private boolean AmFM; //indica en qué frecuencia está
+	private boolean amFM; //indica en qué frecuencia está
 
 	//Metodos------------------------------------------
 
+	//Cambiar de Am a Fm o viceversa
+	public void cambiarFrecuencia(boolean amFM){
+		
+	}
+	//Cambiar estado de la radio 
+	public void onOff(boolean encendido){ 
 
+	}
+	//Cambiar de estacion
+	public void avanzar(String[] estacion){
+
+	}
+	//Guardar emisoras (12)
+	public guardar(String [] favAm, String [] favFM){
+		String [] favAm = new favAm[12]; 
+		String [] favFM = new favFM[12];
+	}
+	//Seleccioar una emisora fav
+	public seleccionarEmisora(){
+
+	}
+	//Indica la estacion en la que se encuentra
+	public String estacionActual(){
+
+	}
+	// Indica si esta encendido o apagado 
+	public boolean estado(){
+
+	}
+
+
+
+String[] oponente = {"Speedyngi", "Jozhingo", "Bongingo", "Zebroar"};
+	int posicion = aleatorio.nextInt(3);
 }
