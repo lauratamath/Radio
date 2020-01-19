82842b8f88e3fe7464960b9581ee42f07e0f443f
lauratamath <laurylevi@hotmail.com>
2020-01-19 10:20:27 -0600

    .

--- Main.java
+++ Main.java
@@ -1,2 +1,8 @@
 //Laura Tamath 19365
 //Orlando Cabrera 19943
+
+public class Main{
+	public static void main (String args []){
+		
+	}
+}
--- RadioLo.java
+++ RadioLo.java
@@ -1,6 +1,16 @@
 //Laura Tamath 19365
 //Orlando Cabrera 19943
 
+import java.util.Random; //Al encenderlo, tira un número aleatorio de alguna estación
 public class RadioLo{
-	
+	//Declaración de variables
+	private boolean encendido;//Indica si la radio esta encendida o apagada
+	private String[] estacion; //Indica las estacione existentes
+	private String[] favFM; //guarda las estaciones favoritas de Fm
+	private String[] favAm; //guarda las estaciones favoritas de Am
+	private boolean AmFM; //indica en qué frecuencia está
+
+	//Metodos------------------------------------------
+
+
 }
